83190c55cba3ce0a5e1f0315bdbe6812cfec224f
semionpiskarev <semion@cs.unc.edu>
2017-11-24 16:42:12 -0500

    Did some initial layout, set up the new item creation and editing but did not yet attach to database

--- clientSide/toDoList.js
+++ clientSide/toDoList.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
    // that the user could make up any kind of AJAX call.   
    } else if (stringIsAlphanumeric(queryString)){
       
+      displayListPage();
+      
       // Make an AJAX call to our REST interface to get the list items 
       $.ajax("./serverSide/RestInterface.php/" + queryString,{
          type: "GET",
          dataType: "json",
-         success: onListLoad,
+         success: onListLoad, // fills in title, list items, activates buttons
          error: displayListNotFound
       });
       
@@ -20,6 +22,72 @@ $(document).ready(function(){
       displayListInvalid();
    }
 });
+
+function createLandingPage(){
+   
+}
+
+// Sets up the page for displaying a list.
+function displayListPage(){
+   // Add placeholder for title
+   $(".page_body").append('<h2 class="list_name">My List</h2>');
+   
+   // Create place to add items
+   $(".page_body").append('<div class="container to_do_list"></div>');
+   
+   // Add the footer   
+   $("body").append('\
+      <footer class="button_footer">\
+         <div class="button_footer__contents">\
+            <div class="container d-flex justify-content-center">\
+               <button type="footer_button" class="btn btn-success btn-md button_footer__btn create_new_item_btn">\
+                  <i class="fa fa-plus"></i> Add Item\
+               </button>\
+               <button type="button" class="btn btn-danger btn-md button_footer__btn record_new_item_btn">\
+                  <i class="fa fa-microphone"></i> Record\
+               </button>\
+            </div>\
+         </div>\
+      </footer>'
+      );
+      
+   // Add handlers to the footer buttons
+   $(".create_new_item_btn").on("click", newItemButtonHandler);
+}
+
+// Populates todo list once AJAX call gets through.
+function onListLoad(data, textStatus, jqXHR){
+   
+   $("list_name").contents(data.name);
+   
+   data.items.forEach(function(element, index){
+      addListItem(element.content, index);
+   });
+}
+
+// Adds an item card to the to do list container
+function addListItem(contents, orderIndex){
+   var newItem = $(
+      '<div class="card item_card">\
+         <div class="row  item_card__row">\
+            <div class="col-2 col-md-1 d-flex justify-content-end align-items-center">\
+               <div class="item_card__checkbox d-flex justify-content-center align-items-center">\
+                  <i class="fa fa-check icon-large item_card__checkmark"></i>\
+               </div>\
+            </div>\
+            <div class="col-7 col-md-9 d-flex align-items-center item_card__text">' + contents + '</div>\
+            <div class="col-3 col-md-2 d-flex justify-content-end">\
+               <button type="button" class="btn btn-default btn-sm item_card__btn item_card__edit_btn"><i class="fa fa-edit"></i></button>\
+               <button type="button" class="btn btn-danger btn-sm item_card__btn item_card__delete_btn"><i class="fa fa-times"></i></button>\
+            </div>\
+         </div>\
+      </div>'
+      );
+   $(".to_do_list").append(newItem);
+   newItem.data("order", orderIndex);
+   newItem.find(".item_card__edit_btn").on("click", editItemHandler);
+   return newItem;
+}
 
 // Helper function for finding whether a string is alphanumeric. Can also be
 // done with a regex, but probably won't be any faster.
@@ -37,24 +105,52 @@ function stringIsAlphanumeric(someString){
    return true;
 }
 
-function createLandingPage(){
+
+function newItemButtonHandler(event){
    
+   // Figure out how many items there are already
+   var newOrderNumber = $(".item_card").length;
+   
+   // Create an item
+   var newItemCard = addListItem("", newOrderNumber);
+   makeItemEditable(newItemCard);
+}
+
+// Handles button click on an "edit" button of a list item
+function editItemHandler(event){
+   makeItemEditable($(this).closest(".item_card"));
+}
+
+// Makes a particular item card text editable (for editing or after creation)
+function makeItemEditable(itemCard){
+   var $itemContents = itemCard.find(".item_card__text").first();
+   var itemText = $itemContents.text();
+   var $textArea = $('<textArea class="col-7 col-md-9 item_card__text"></textArea>');
+   $itemContents.replaceWith($textArea);
+   $textArea.focus();
+   $textArea.val(itemText);
+   $textArea.on("blur", doneEditingItem);
+}
+
+// Saves an edited item
+function doneEditingItem(event){
+
+   var $textArea = $(this);
+   var newItemText = $textArea.val();
+   var itemCard = $textArea.closest(".item_card");
+   var itemOrderIndex = itemCard.data("order");
+   
+   // TODO: Save item to the database
+   
+   // Replace input element with text
+   $textArea.replaceWith('<div class="col-7 col-md-9 d-flex align-items-center item_card__text">' + newItemText + '</div>')
 }
 
 function displayListInvalid(){
-   console.log("no")
+   
 }
 
 function displayListNotFound(){
    // TODO: needs to be changed
    $("div").text("List not found");
-}
-
-// Function to execute once AJAX call to get a list goes through
-function onListLoad(data, textStatus, jqXHR){
-   
-   data.items.forEach(function(element, index){
-      $(".to_do_list").append("<li>" + element.content + "</li>");
-   });
-
 }
--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
    <head>
-      <title>To Do With Voice</title>
-      <meta charset="UTF-8">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <!-- Required meta tags for Bootstrap -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      
+      <!-- Our CSS -->
+      <link rel="stylesheet" href="clientSide/toDoList.css">
+    
+      <!-- Scripts with defer will only execute after html is done parsing. These can be placed at the bottom
+      of the body element instead as well, but might start being downloaded later. -->
+      <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+      <!-- Scripts for Bootstrap -->
+      <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+      <script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+      
+      <!-- Our scripts. Careful- we can't defer inline JS, so jQuery is not available inline -->
       <script>
          var queryString = "<?php print($_SERVER['QUERY_STRING']); ?>";
       </script>
-      <script src="clientSide/toDoList.js"></script>
+      <script defer src="clientSide/toDoList.js"></script>
    </head>
    <body>
-      <div>To Do items:</div>
-      <ul class="to_do_list"></ul>
-        
+      <nav class="navbar navbar-dark">
+         <span class="navbar-brand">Do-It</a>
+      </nav>
+      
+      <div class="page_body"></div>
    </body>
 </html>
